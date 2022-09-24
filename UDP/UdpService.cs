e09bc6ff62171091daa38b1d1c2e209b04832b48
Gabriel Zanin <gabriel.zanin@solidinvent.com.br>
2022-09-24 17:49:10 -0300

    first commit

---
+++ UDP/UdpService.cs
@@ -0,0 +1,42 @@
+﻿namespace UDP;
+
+public class UdpService
+{
+        
+    public static UdpClient UdpClient 
+    {
+        get
+        {
+            if (_udpClient == null)
+                _udpClient = new UdpClient();
+            return _udpClient;
+        }
+    }
+    private static UdpClient _udpClient { get; set; }
+    public static Socket Socket
+    {
+        get
+        {
+            if (Socket == null)
+                _socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            return _socket;
+        }
+    }
+    private static Socket _socket { get; set; }
+    public static async Task Send(Byte[] message, IPEndPoint address)
+    {
+        try
+        {
+            Socket.SendTo(message, address);
+        }
+        catch(Exception e)
+        {
+
+        }
+    }
+        
+    public static async Task Listen()
+    {
+
+    }
+}
---
+++ UDP/Usings.cs
@@ -0,0 +1,7 @@
+﻿global using System.Net.Sockets;
+global using System.Net;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
